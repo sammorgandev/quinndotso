0f01cad8a3e09532e18b1901a9b3b7381a7cca40
Samuel Morgan <samuelmorgan@Samuels-MacBook-Pro.local>
2024-01-02 20:12:33 -0500

    Fixed typescript errors

--- components/dashboard/StatRow.tsx
+++ components/dashboard/StatRow.tsx
@@ -2,13 +2,14 @@ import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid'
 import { CursorArrowRaysIcon, EnvelopeOpenIcon, UsersIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 import '../../app/globals.css'
+
 const stats = [
   { id: 1, name: 'Total Subscribers', stat: '71,897', icon: UsersIcon, change: '122', changeType: 'increase' },
   { id: 2, name: 'Avg. Open Rate', stat: '58.16%', icon: EnvelopeOpenIcon, change: '5.4%', changeType: 'increase' },
   { id: 3, name: 'Avg. Click Rate', stat: '24.57%', icon: CursorArrowRaysIcon, change: '3.2%', changeType: 'decrease' },
 ]
 
-function classNames(...classes) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
